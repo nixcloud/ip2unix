6460ead5f9ecc17da1abd2cd3390d11e14df61ef
aszlig <aszlig@nix.build>
2018-10-23 19:30:07 +0200

    Use explicit function type signatures for DlsymFun
    So far we have used decltype() to look up the function signature, which
    works fine as long as we don't have a clash between the type signature
    of a function definition in preload.cc.
    
    While this makes realcalls.hh a bit more verbose, it does have the
    advantage that we require fewer headers and also avoid those collisions
    mentioned.
    
    I also sorted the symbols alphabetically.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- src/realcalls.hh
+++ src/realcalls.hh
@@ -3,10 +3,9 @@
 #define IP2UNIX_REALCALLS_HH
 
 #include <mutex>
-#include <unistd.h>
 
+#include <unistd.h>
 #include <dlfcn.h>
-#include <sys/socket.h>
 
 /* Let's declare all of the wrappers as extern, so that we can define them by
  * simply overriding IP2UNIX_REALCALL_EXTERN before the #include directive.
@@ -22,10 +21,10 @@ extern std::mutex g_dlsym_mutex;
  * close we can just use real::close(fd).
  */
 namespace real {
-    template <typename Sig, typename Self>
+    template <typename Self, typename Ret, typename ... FunArgs>
     struct DlsymFun
     {
-        Sig fptr = nullptr;
+        Ret (*fptr)(FunArgs ...) = nullptr;
 
         template <typename ... Args>
         auto operator()(Args ... args) -> decltype(fptr(args ...))
@@ -39,30 +38,30 @@ namespace real {
                     g_dlsym_mutex.unlock();
                     _exit(EXIT_FAILURE);
                 }
-                this->fptr = reinterpret_cast<Sig>(result);
+                this->fptr = reinterpret_cast<decltype(fptr)>(result);
             }
             g_dlsym_mutex.unlock();
             return this->fptr(args ...);
         }
     };
 
-#define DLSYM_FUN(name) IP2UNIX_REALCALL_EXTERN \
-    struct name##_fun_t : public DlsymFun<decltype(&::name), name##_fun_t> { \
+#define DLSYM_FUN(name, ...) IP2UNIX_REALCALL_EXTERN \
+    struct name##_fun_t : public DlsymFun<name##_fun_t, __VA_ARGS__> { \
         static constexpr const char *fname = #name; \
     } name
 
-    DLSYM_FUN(socket);
-    DLSYM_FUN(setsockopt);
-    DLSYM_FUN(bind);
-    DLSYM_FUN(connect);
+    DLSYM_FUN(accept, int, int, struct sockaddr*, socklen_t*);
+    DLSYM_FUN(accept4, int, int, struct sockaddr*, socklen_t*, int);
+    DLSYM_FUN(bind, int, int, const struct sockaddr*, socklen_t);
+    DLSYM_FUN(close, int, int);
+    DLSYM_FUN(connect, int, int, const struct sockaddr*, socklen_t);
+    DLSYM_FUN(getpeername, int, int, struct sockaddr*, socklen_t*);
+    DLSYM_FUN(getsockname, int, int, struct sockaddr*, socklen_t*);
 #ifdef SOCKET_ACTIVATION
-    DLSYM_FUN(listen);
+    DLSYM_FUN(listen, int, int, int);
 #endif
-    DLSYM_FUN(accept);
-    DLSYM_FUN(accept4);
-    DLSYM_FUN(getpeername);
-    DLSYM_FUN(getsockname);
-    DLSYM_FUN(close);
+    DLSYM_FUN(setsockopt, int, int, int, int, const void*, socklen_t);
+    DLSYM_FUN(socket, int, int, int, int);
 }
 
 #endif
