0c83dcfce3b6cd7b46cffa4c3fd09bdcb93739a9
aszlig <aszlig@nix.build>
2018-10-30 09:35:43 +0100

    Add missing test for "reject" rule option
    When implementing that option I actually wrote the test first but forgot
    to actually add it to the commit.
    
    Signed-off-by: aszlig <aszlig@nix.build>

---
+++ tests/test_reject.py
@@ -0,0 +1,41 @@
+import subprocess
+import sys
+
+from helper import IP2UNIX
+
+TESTPROG = '''
+import socket
+import errno
+
+from contextlib import contextmanager
+
+@contextmanager
+def assert_reject(errno):
+    try:
+        yield
+    except OSError as e:
+        assert e.errno == errno, '{} != {}'.format(e.errno, errno)
+        return
+    raise AssertionError("not risen")
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
+    with assert_reject(errno.EPERM):
+        server.bind(('127.0.0.1', 1234))
+
+    with assert_reject(errno.EPERM):
+        server.connect(('127.0.0.1', 1234))
+
+with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as server:
+    with assert_reject(errno.EACCES):
+        server.bind(('1234::1', 1234))
+
+    with assert_reject(errno.EACCES):
+        server.connect(('1234::1', 1234))
+'''
+
+
+def test_sendrecv_direct():
+    rules = ['-r', 'addr=1234::1,reject', '-r', 'reject=eperm']
+    cmd = [IP2UNIX] + rules + [sys.executable, '-c', TESTPROG]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    assert result.returncode == 0
