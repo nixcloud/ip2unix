d2ff3387817870edca749e0cbc1f675c2e0090ff
aszlig <aszlig@nix.build>
2018-11-04 13:51:49 +0100

    BlackHole: Try harder to find temporary directory
    So far we just did a look up on the TMPDIR environment variable and if
    that has failed, we just set /tmp as the temporary directory. In theory
    this should work on most GNU/Linux systems, but let's make sure we test
    a few other locations.
    
    Apart from testing a range of environment variables we still fall back
    to /tmp and /var/tmp but also the current working directory. We now also
    test whether the directory is writable and if it's actually really a
    directory by also resolving the realname.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- src/blackhole.cc
+++ src/blackhole.cc
@@ -1,21 +1,86 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 #include <cstring>
+#include <climits>
 #include <unistd.h>
 
+#include <sys/stat.h>
+
 #include "blackhole.hh"
+
+static std::optional<std::string> getenv_str(const std::string &envar)
+{
+    const char *result;
+    result = getenv(envar.c_str());
+    if (result == nullptr)
+        return std::nullopt;
+    return std::string(result);
+}
+
+static bool is_writable_dir(const std::string &dir)
+{
+    int old_errno = errno;
+
+    char buf[PATH_MAX];
+    char *resolved = realpath(dir.c_str(), buf);
+
+    if (resolved == nullptr) {
+        errno = old_errno;
+        return false;
+    }
+
+    if (access(resolved, W_OK) == -1) {
+        errno = old_errno;
+        return false;
+    }
+
+    struct stat st;
+
+    if (stat(resolved, &st) == -1) {
+        errno = old_errno;
+        return false;
+    }
+
+    return S_ISDIR(st.st_mode);
+}
+
+static std::string get_tmpdir(void)
+{
+    for (const std::string &tryenv : {"TMPDIR", "TMP", "TEMP", "TEMPDIR"}) {
+        std::optional<std::string> tmpdir = getenv_str(tryenv);
+        if (!tmpdir || !is_writable_dir(tmpdir.value()))
+            continue;
+
+        return tmpdir.value();
+    }
+
+    if (is_writable_dir("/tmp"))
+        return "/tmp";
+
+    if (is_writable_dir("/var/tmp"))
+        return "/var/tmp";
+
+    int old_errno = errno;
+    char *workdir = get_current_dir_name();
+    errno = old_errno;
+    if (workdir != nullptr) {
+        std::string wdir_str(workdir);
+        free(workdir);
+        if (is_writable_dir(workdir))
+            return workdir;
+    }
+
+    fputs("FATAL: Unable to get temporary directory.\n", stderr);
+    std::abort();
+}
+
 
 BlackHole::BlackHole()
     : tmpdir(std::nullopt)
     , filepath(std::nullopt)
 {
-    static const char *tempdir = nullptr;
+    static std::string tempdir = get_tmpdir();
 
-    if (tempdir == nullptr) {
-        if ((tempdir = getenv("TMPDIR")) == nullptr)
-            tempdir = "/tmp";
-    }
-
-    std::string bh_template = std::string(tempdir) + "/ip2unix.XXXXXX";
+    std::string bh_template = tempdir + "/ip2unix.XXXXXX";
 
     char *c_bh_template = strdup(bh_template.c_str());
 
