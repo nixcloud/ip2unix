nixcloud/ip2unix

commit 0789c18a9c7bd17df5948da63185d15daeefac70

Prevent closing file descriptors passed by systemd
Some applications tend to try closing all the file descriptors except
maybe the stdio ones (0, 1 and 2), so when using a rule with socket
activation enabled it could try to re-use a file descriptor that has
already been closed.

In order to prevent this, we now check during close() whether the file
descriptor is passed by systemd and if it is, we simply turn close()
into a no-op.

Along the way, I moved most of the systemd handling into its own
namespace, because a file like rules/match.cc probably doesn't tell a
whole lot about it being about systemd (the file used to have more than
just the get_systemd_fd_for_rule function).

Signed-off-by: aszlig <aszlig@nix.build>

meson.build
@@ -20,20 +20,11 @@ add_project_arguments(cc.get_supported_arguments(warning_flags),
 
 python = import('python').find_installation()
 
-sources = []
-includes = []
-subdir('src')
-
-generate_sym_map = [python, files('meson/gensyms.py'), '@INPUT@']
-sym_map = custom_target('symmap', input: sources, output: 'symbols.map',
-                        command: generate_sym_map, capture: true)
-
 cflags = ['-fPIC']
 
 ldflags = [
   '-pie',
   '-Wl,-E',
-  '-Wl,--version-script,@0@'.format(sym_map.full_path()),
 ]
 
 deps = [
@@ -59,6 +50,15 @@ if libsystemd.found()
     cflags += ['-DNO_FDNAMES']
   endif
 endif
+
+sources = []
+includes = []
+subdir('src')
+
+generate_sym_map = [python, files('meson/gensyms.py'), '@INPUT@']
+sym_map = custom_target('symmap', input: sources, output: 'symbols.map',
+                        command: generate_sym_map, capture: true)
+cflags += ['-Wl,--version-script,@0@'.format(sym_map.full_path())]
 
 ip2unix = executable('ip2unix', sources, install: true, dependencies: deps,
                      link_args: ldflags, link_depends: sym_map,

src/meson.build
@@ -16,11 +16,13 @@ sources += files('ip2unix.cc',
                  'blackhole.cc',
                  'preload.cc',
                  'realcalls.cc',
-                 'rules/match.cc',
                  'rules/parse.cc',
                  'socket.cc',
                  'sockaddr.cc',
                  'sockopts.cc')
+if libsystemd.found()
+  sources += files('systemd.cc')
+endif
 sources += dynports_sources
 sources += errnos
 includes += include_directories('.')

src/preload.cc
@@ -18,6 +18,10 @@
 #include "realcalls.hh"
 #include "socket.hh"
 
+#ifdef SOCKET_ACTIVATION
+#include "systemd.hh"
+#endif
+
 static std::mutex g_rules_mutex;
 
 static std::shared_ptr<const std::vector<Rule>> g_rules = nullptr;
@@ -42,6 +46,16 @@ static void init_rules(void)
 
     if (!rules)
         _exit(EXIT_FAILURE);
+
+#ifdef SOCKET_ACTIVATION
+    for (const Rule &rule : rules.value()) {
+        if (!rule.socket_activation)
+            continue;
+
+        Systemd::init();
+        break;
+    }
+#endif
 
     g_rules = std::make_shared<std::vector<Rule>>(rules.value());
 }
@@ -172,7 +186,7 @@ static inline int bind_connect(SockFun &&sockfun, RealFun &&realfun,
 
 #ifdef SOCKET_ACTIVATION
         if (rule.value().socket_activation) {
-            std::optional<int> newfd = get_systemd_fd_for_rule(rule.value());
+            std::optional<int> newfd = Systemd::get_fd_for_rule(rule.value());
             if (newfd) {
                 return sock->activate(inaddr, newfd.value());
             } else {
@@ -426,6 +440,15 @@ extern "C" int WRAP_SYM(dup3)(int oldfd, int newfd, int flags)
 
 extern "C" int WRAP_SYM(close)(int fd)
 {
+#ifdef SOCKET_ACTIVATION
+    {
+        std::scoped_lock<std::mutex> lock(g_rules_mutex);
+        init_rules();
+        if (Systemd::has_fd(fd))
+            return 0;
+    }
+#endif
+
     return Socket::when<int>(fd, [&](Socket::Ptr sock) {
         return sock->close();
     }, [&]() {

src/rules.hh
@@ -38,8 +38,4 @@ std::optional<Rule> parse_rule_arg(size_t, const std::string&);
 std::string encode_rules(std::vector<Rule>);
 void print_rules(std::vector<Rule>&, std::ostream&);
 
-#ifdef SOCKET_ACTIVATION
-std::optional<int> get_systemd_fd_for_rule(const Rule&);
-#endif
-
 #endif

src/systemd.cc
@@ -1,42 +1,38 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-#include "../rules.hh"
-
 #include <cstring>
 #include <unordered_map>
 #include <queue>
 
-#include <arpa/inet.h>
-
-#ifdef SOCKET_ACTIVATION
 #include <systemd/sd-daemon.h>
 
-/*
- * Get a systemd socket file descriptor for the given rule either via name if
- * fd_name is set or just the next file descriptor available.
- */
-std::optional<int> get_systemd_fd_for_rule(const Rule &rule)
+#include "rules.hh"
+#include "systemd.hh"
+
+static std::unordered_map<std::string, int> names;
+static std::queue<int> fds;
+static int fd_count;
+
+void Systemd::init(void)
 {
-    static std::unordered_map<std::string, int> names;
-    static std::queue<int> fds;
     static bool fetch_done = false;
 
     if (!fetch_done) {
         char **raw_names = nullptr;
 #ifdef NO_FDNAMES
-        int count = sd_listen_fds(1);
+        fd_count = sd_listen_fds(1);
 #else
-        int count = sd_listen_fds_with_names(1, &raw_names);
+        fd_count = sd_listen_fds_with_names(1, &raw_names);
 #endif
-        if (count < 0) {
+        if (fd_count < 0) {
             fprintf(stderr, "FATAL: Unable to get systemd sockets: %s\n",
                     strerror(errno));
             std::abort();
-        } else if (count == 0) {
+        } else if (fd_count == 0) {
             fputs("FATAL: Needed at least one systemd socket file descriptor,"
                   " but found zero.\n", stderr);
             std::abort();
         }
-        for (int i = 0; i < count; ++i) {
+        for (int i = 0; i < fd_count; ++i) {
 #ifdef NO_FDNAMES
             fds.push(SD_LISTEN_FDS_START + i);
 #else
@@ -51,7 +47,14 @@ std::optional<int> get_systemd_fd_for_rule(const Rule &rule)
             free(raw_names);
         fetch_done = true;
     }
+}
 
+/*
+ * Get a systemd socket file descriptor for the given rule either via name if
+ * fd_name is set or just the next file descriptor available.
+ */
+std::optional<int> Systemd::get_fd_for_rule(const Rule &rule)
+{
 #ifndef NO_FDNAMES
     if (rule.fd_name) {
         auto found = names.find(rule.fd_name.value());
@@ -72,4 +75,9 @@ std::optional<int> get_systemd_fd_for_rule(const Rule &rule)
     fds.pop();
     return fd;
 }
-#endif
+
+/* Check whether the given file descriptor is passed by systemd. */
+bool Systemd::has_fd(int fd)
+{
+    return fd >= SD_LISTEN_FDS_START && fd < SD_LISTEN_FDS_START + fd_count;
+}

src/systemd.hh
@@ -1,0 +1,11 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+#ifndef IP2UNIX_SYSTEMD_HH
+#define IP2UNIX_SYSTEMD_HH
+
+namespace Systemd {
+    void init(void);
+    std::optional<int> get_fd_for_rule(const Rule&);
+    bool has_fd(int);
+}
+
+#endif

tests/test_closeallfds.py
@@ -1,0 +1,52 @@
+import sys
+import subprocess
+import socket
+import time
+
+import helper
+
+TESTPROG = r'''
+import os
+import sys
+import socket
+
+for i in range(3, 1024):
+    try:
+        os.close(i)
+    except OSError:
+        pass
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind(('9.8.7.6', 1234))
+    sock.listen(10)
+    with sock.accept()[0] as conn:
+        data = conn.recv(10)
+        conn.sendall(data.upper())
+
+sys.stdout.write('DONE\n')
+sys.stdout.flush()
+'''
+
+
+@helper.systemd_sa_helper_only
+def test_closeallfds(tmpdir):
+    sockfile = str(tmpdir.join('test.sock'))
+    cmd = [
+        helper.SYSTEMD_SA_PATH, '-l', sockfile,
+        helper.IP2UNIX, '-r', 'systemd',
+        sys.executable, '-c', TESTPROG
+    ]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE) as server:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+            while True:
+                try:
+                    client.connect(sockfile)
+                except FileNotFoundError:
+                    pass
+                else:
+                    break
+                time.sleep(0.1)
+            client.sendall(b'abcdefghij')
+            assert client.recv(10) == b'ABCDEFGHIJ'
+        stdout = server.communicate(timeout=5)[0]
+        assert stdout == b'DONE\n'

commit b731a64c2ae28e572a09ab17a658138848a0b141

meson: Fix passing linker script to the linker
Regression introduced by 0789c18a9c7bd17df5948da63185d15daeefac70.

The commit in question moved the systemd handling into its dedicated
file and the Meson restructuring accidentally moved the --version-script
argument from ldflags to cflags.

This unfortunately means that since this commit (and thus version 1.1.0)
we're exporting too many symbols.

To make sure this doesn't happen again in the future, I added a small
check to the installCheckPhase of the main derivation, which checks the
expected exported symbols against the ones actually being exported.

Signed-off-by: aszlig <aszlig@nix.build>

default.nix
@@ -47,5 +47,12 @@ pkgs.stdenv.mkDerivation rec {
       echo "ERROR: Manual page hasn't been generated." >&2
       exit 1
     fi
+
+    diff -u <(
+      find "$src/src" -iname '*.cc' -type f -exec \
+        sed -n -e '/^ *#/!s/^.*WRAP_SYM(\([^)]\+\)).*/\1/p' {} + | sort
+    ) <(
+      nm --defined-only -g "$out/lib/libip2unix.so" | cut -d' ' -f3- | sort
+    )
   '';
 }

meson.build
@@ -30,6 +30,7 @@ cflags = ['-DVERSION="' + meson.project_version() + '"']
 cflags += ['-fPIC']
 main_cflags = []
 lib_cflags = []
+lib_ldflags = []
 
 deps = [
   dependency('yaml-cpp', version: '>=0.5.0'),
@@ -57,11 +58,12 @@ subdir('src')
 generate_sym_map = [python, script_gensyms, '@INPUT@']
 sym_map = custom_target('symmap', input: lib_sources, output: 'symbols.map',
                         command: generate_sym_map, capture: true)
-cflags += ['-Wl,--version-script,@0@'.format(sym_map.full_path())]
+lib_ldflags += ['-Wl,--version-script,@0@'.format(sym_map.full_path())]
 
 libip2unix = shared_library('ip2unix', lib_sources, install: true,
                             dependencies: deps, link_depends: sym_map,
                             cpp_args: lib_cflags + cflags,
+                            link_args: lib_ldflags,
                             include_directories: includes)
 
 ip2unix = executable('ip2unix', main_sources, install: true,