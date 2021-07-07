nixcloud/ip2unix

commit 1a8a9b6ca73865a10452eb10c93dd751d0ba6af7

systemd: Improve state handling for passed fds
So far, we re-used LISTEN_FDS and LISTEN_FDNAMES in order to assign the
systemd socket file descriptors to the rules. We did that using a queue
which holds all the file descriptors and another unordered_map that
holds just the named file descriptions.

However, this has a few problems:

  * In multi-process applications we might end up assigning the same
    file descriptors twice, if the process forks off and uses execve().
  * If an unassigned file descriptor overlaps with a mapped one, the
    mapped file descriptor could become unavailable or blocked once
    we're doing a mapped lookup.
  * Systemd file descriptors aren't properly closed after being in use
    and the program uses execve().

While the last item isn't fully addressed by this, the fix however is
rather easy to do and will be following soon.

The main thing we're addressing now are the details on how sockets get
associated to file descriptors passed by systemd. We now associate the
systemd file descriptors to the rules during initialisation, whereas
before we did it on every lookup.

Those differences are better explained with an example, so consider we
have the following environment variables:

  * LISTEN_FDS=5
  * LISTEN_FDNAMES=???:foo:bar:???:???

With the old and new method this buils down to the following:

  Without rule-aware processing   |       Rule-aware processing
 ---------------------------------|------------------------------------
 .-----.-----.-----.-----.-----.  |  .-----.-----.-----.  .-----.-----.
 | ??? | foo | bar | ??? | ??? |  |  | ??? | ??? | ??? |  | foo | bar |
 |  0  |  1  |  2  |  3  |  4  |  |  |  0  |  3  |  4  |  |  1  |  2  |
 `-----'-----'--.--'-----'-----'  |  `-----'-----'-----'  `-----'--.--'
                |                 |                                |
       acquire fdname "bar"       |                acquire fdname "bar"
                |                 |                                |

src/preload.cc
@@ -56,11 +56,11 @@ static void init_rules(void)
         _exit(EXIT_FAILURE);
 
 #ifdef SYSTEMD_SUPPORT
-    for (const Rule &rule : rules.value()) {
+    for (const Rule &rule : *rules) {
         if (!rule.socket_activation)
             continue;
 
-        Systemd::init();
+        Systemd::init(*rules);
         break;
     }
 #endif
@@ -208,7 +208,8 @@ static inline int bind_connect(SockFun &&sockfun, RealFun &&realfun,
 
 #ifdef SYSTEMD_SUPPORT
         if (rule->second.socket_activation) {
-            std::optional<int> newfd = Systemd::get_fd_for_rule(rule->second);
+            std::optional<int> newfd =
+                Systemd::acquire_fd_for_rulepos(rule->first);
             if (newfd) {
                 return sock->activate(inaddr, newfd.value());
             } else {

src/serial.hh
@@ -2,7 +2,9 @@
 #ifndef IP2UNIX_SERIAL_HH
 #define IP2UNIX_SERIAL_HH
 
+#include <deque>
 #include <sstream>
+#include <unordered_map>
 
 #include "rules.hh"
 
@@ -23,14 +25,14 @@ MaybeError deserialise(std::istream&, SocketType*);
 void serialise(const Rule&, std::ostream&);
 MaybeError deserialise(std::istream&, Rule*);
 
-inline void serialise(const int &val, std::ostream &out)
+template <typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
+inline void serialise(const T &val, std::ostream &out)
 {
     out << val;
     out.put('&');
 }
 
-template <typename T,
-          typename std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+template <typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
 MaybeError deserialise(std::istream &in, T *out)
 {
     char c;
@@ -67,6 +69,26 @@ MaybeError deserialise(std::istream &in, std::optional<T> *out)
 }
 
 template <typename T>
+void serialise(const std::deque<T> &val, std::ostream &out)
+{
+    for (const T &item : val)
+        serialise(item, out);
+}
+
+template <typename T>
+MaybeError deserialise(std::istream &in, std::deque<T> *out)
+{
+    while (in.peek() != EOF) {
+        T outval;
+        MaybeError err;
+        if (err = deserialise(in, &outval))
+            return err;
+        out->push_back(outval);
+    }
+    return std::nullopt;
+}
+
+template <typename T>
 void serialise(const std::vector<T> &val, std::ostream &out)
 {
     for (const T &item : val)
@@ -82,6 +104,45 @@ MaybeError deserialise(std::istream &in, std::vector<T> *out)
         if (err = deserialise(in, &outval))
             return err;
         out->push_back(outval);
+    }
+    return std::nullopt;
+}
+
+template <typename K, typename V>
+void serialise(const std::unordered_map<K, V> &val, std::ostream &out)
+{
+    for (const std::pair<K, V> &item : val) {
+        serialise(item.first, out);
+        out.put('=');
+        serialise(item.second, out);
+        out.put(';');
+    }
+}
+
+template <typename K, typename V>
+MaybeError deserialise(std::istream &in, std::unordered_map<K, V> *out)
+{
+    char c;
+    while (in.peek() != EOF) {
+        MaybeError err;
+
+        K outkey;
+        if (err = deserialise(in, &outkey))
+            return err;
+
+        if ((c = in.get()) != '=')
+            return std::string("Invalid character '") + c + "' after map key.";
+
+        V outval;
+        if (err = deserialise(in, &outval))
+            return err;
+
+        if ((c = in.get()) != ';') {
+            return std::string("Invalid character '") + c
+                 + "' after map record.";
+        }
+
+        (*out)[outkey] = outval;
     }
     return std::nullopt;
 }

src/systemd.cc
@@ -1,23 +1,146 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 #include <cstring>
 #include <unordered_map>
-#include <queue>
+#include <unordered_set>
+#include <deque>
 
 #include "rules.hh"
 #include "systemd.hh"
 #include "logging.hh"
+#include "serial.hh"
 
 #define SD_LISTEN_FDS_START 3
 
-static std::unordered_map<std::string, int> names;
-static std::queue<int> fds;
-static int fd_count = 0;
+static std::unordered_map<size_t, int> fdmap;
+static std::deque<int> fds;
+static std::unordered_set<int> all_fds;
 
-void Systemd::init(void)
+/* Fetch a colon-separated environment variable and split it into a vector. */
+static std::vector<std::string> get_env_vector(const char *name)
+{
+    const char *ptr;
+    std::vector<std::string> result;
+
+    const char *value = getenv(name);
+
+    if (value == nullptr || *value == '\0')
+        return result;
+
+    LOG(DEBUG) << "Splitting value of " << name << ": " << value;
+
+    while ((ptr = strchr(value, ':')) != nullptr) {
+        using lentype = std::string::size_type;
+        lentype len = static_cast<lentype>(ptr - value);
+        std::string elem(value, len);
+        LOG(DEBUG) << "Got element '" << elem << "' from " << name << '.';
+        result.push_back(std::string(value, len));
+        value = ptr + 1;
+    }
+
+    LOG(DEBUG) << "Got last element '" << value << "' from " << name << '.';
+    result.push_back(std::string(value));
+    return result;
+}
+
+static std::string join(const std::string &delim,
+                        const std::vector<std::string> &chunks)
+{
+    std::string result;
+
+    for (const std::string &chunk : chunks) {
+        if (!result.empty())
+            result += delim;
+        result += chunk;
+    }
+
+    return result;
+}
+
+/*
+ * Update the bookkeeping environment variables needed to associate systemd
+ * socket file descriptors to rules.
+ */
+static void update_env(void)
+{
+    int ret;
+
+    std::string fdval = serialise(fds);
+
+    LOG(DEBUG) << "Setting __IP2UNIX_SYSTEMD_FDS to '"
+               << fdval << "'.";
+
+    ret = setenv("__IP2UNIX_SYSTEMD_FDS", fdval.c_str(), 1);
+    if (ret == -1) {
+        LOG(FATAL) << "Unable to set __IP2UNIX_SYSTEMD_FDS: "
+                   << strerror(errno);
+        std::abort();
+    }
+
+    std::string fdmapval = serialise(fdmap);
+    LOG(DEBUG) << "Setting __IP2UNIX_SYSTEMD_FDMAP to '"
+               << fdmapval << "'.";
+
+    ret = setenv("__IP2UNIX_SYSTEMD_FDMAP", fdmapval.c_str(), 1);
+    if (ret == -1) {
+        LOG(FATAL) << "Unable to set __IP2UNIX_SYSTEMD_FDMAP: "
+                   << strerror(errno);
+        std::abort();
+    }
+}
+
+/*
+ * Reinitialises the data structures from previous values written by
+ * update_env().
+ */
+static bool init_from_env(void)
+{
+    MaybeError err;
+    const char *fds_raw = getenv("__IP2UNIX_SYSTEMD_FDS");
+    const char *fdmap_raw = getenv("__IP2UNIX_SYSTEMD_FDMAP");
+
+    if (fds_raw == nullptr || fdmap_raw == nullptr)
+        return false;
+
+    if (err = deserialise(std::string(fds_raw), &fds)) {
+        LOG(FATAL) << "Unable to deserialise __IP2UNIX_SYSTEMD_FDS: "
+                   << *err;
+        std::abort();
+    }
+
+    if (err = deserialise(std::string(fdmap_raw), &fdmap)) {
+        LOG(FATAL) << "Unable to deserialise __IP2UNIX_SYSTEMD_FDMAP: "
+                   << *err;
+        std::abort();
+    }
+
+    LOG(INFO) << "Reinitialising systemd file descriptors from internal"
+              << " __IP2UNIX_SYSTEMD_FD* variables.";
+
+    for (const std::pair<size_t, int> &item : fdmap) {
+        LOG(DEBUG) << "Got systemd file descriptor " << item.second
+                   << " connected to rule #" << item.first << '.';
+        all_fds.insert(item.second);
+    }
+
+    for (const int &fd : fds) {
+        LOG(DEBUG) << "Got systemd file descriptor " << fd << '.';
+        all_fds.insert(fd);
+    }
+
+    return true;
+}
+
+void Systemd::init(const std::vector<Rule> &rules)
 {
     static bool fetch_done = false;
 
     if (!fetch_done) {
+        if (init_from_env()) {
+            fetch_done = true;
+            return;
+        }
+
+        size_t fd_count;
         const char *listen_fds = getenv("LISTEN_FDS");
 
         if (listen_fds == nullptr) {
@@ -32,7 +155,7 @@ void Systemd::init(void)
             std::abort();
         }
 
-        if ((fd_count = atoi(listen_fds)) == 0) {
+        if ((fd_count = strtoul(listen_fds, nullptr, 10)) == 0) {
             LOG(FATAL) << "Needed at least one systemd socket file descriptor,"
                        << " but found zero.";
             std::abort();
@@ -41,26 +164,62 @@ void Systemd::init(void)
         LOG(INFO) << "Number of systemd file descriptors found in LISTEN_FDS: "
                   << fd_count;
 
-        const char *listen_fdnames = getenv("LISTEN_FDNAMES");
+        std::vector<std::string> fdnames = get_env_vector("LISTEN_FDNAMES");
+        size_t fdnames_size = fdnames.size();
 
-        for (int i = 0; i < fd_count; ++i) {
-            if (listen_fdnames != nullptr) {
-                const char *delim = strchr(listen_fdnames, ':');
-                std::string name;
-                if (delim == nullptr) {
-                    name = listen_fdnames;
-                } else {
-                    using lentype = std::string::size_type;
-                    lentype len = static_cast<lentype>(delim - listen_fdnames);
-                    name = std::string(listen_fdnames, len);
-                    listen_fdnames = delim + 1;
-                }
-                LOG(DEBUG) << "Got systemd file descriptor named '" << name
-                           << "' (" << SD_LISTEN_FDS_START + i << ").";
-                names[name] = SD_LISTEN_FDS_START + i;
+        if (fdnames_size > 0 && fdnames_size != fd_count) {
+            LOG(WARNING) << "Mismatch between " << fdnames_size
+                         << " element(s) in LISTEN_FDNAMES ("
+                         << join(", ", fdnames) << ") and " << fd_count
+                         << " LISTEN_FDS.";
+        }
+
+        std::unordered_set<int> avail_fds;
+        for (size_t i = 0; i < fd_count; ++i) {
+            int fd = SD_LISTEN_FDS_START + i;
+            avail_fds.insert(fd);
+            all_fds.insert(fd);
+        }
+
+        size_t rulepos = 0;
+        for (const Rule &rule : rules) {
+            if (!rule.socket_activation || !rule.fd_name) {
+                rulepos++;
+                continue;
             }
 
-            fds.push(SD_LISTEN_FDS_START + i);
+            size_t elems = std::min(fdnames_size, fd_count);
+            for (size_t i = 0; i < elems; ++i) {
+                int fd = SD_LISTEN_FDS_START + i;
+
+                if (fdnames[i] == *rule.fd_name) {
+                    LOG(DEBUG) << "Matched systemd file descriptor name '"
+                               << fdnames[i] << "' (fd " << fd << ")"
+                               << " with rule #" << rulepos << '.';
+                    fdmap[rulepos] = fd;
+                    avail_fds.erase(fd);
+                    continue;
+                }
+            }
+
+            rulepos++;
+        }
+
+        std::copy(avail_fds.begin(), avail_fds.end(),
+                  std::inserter(fds, fds.end()));
+
+        update_env();
+
+        if (unsetenv("LISTEN_FDNAMES") == -1) {
+            LOG(FATAL) << "Unable to unset LISTEN_FDNAMES: "
+                       << strerror(errno);
+            std::abort();
+        }
+
+        if (unsetenv("LISTEN_FDS") == -1) {
+            LOG(FATAL) << "Unable to unset LISTEN_FDS: "
+                       << strerror(errno);
+            std::abort();
         }
 
         fetch_done = true;
@@ -72,28 +231,31 @@ void Systemd::init(void)
  * Get a systemd socket file descriptor for the given rule either via name if
  * fd_name is set or just the next file descriptor available.
  */
-std::optional<int> Systemd::get_fd_for_rule(const Rule &rule)
+std::optional<int> Systemd::acquire_fd_for_rulepos(size_t rulepos)
 {
-    if (rule.fd_name) {
-        auto found = names.find(rule.fd_name.value());
-        if (found == names.end()) {
-            LOG(FATAL) << "Can't get systemd socket for '"
-                       << rule.fd_name.value() << "'.";
-            std::abort();
-        }
-        return found->second;
+    using itype = decltype(fdmap)::const_iterator;
+    itype found = fdmap.find(rulepos);
+
+    if (found != fdmap.end()) {
+        int fd = found->second;
+        fdmap.erase(found);
+        all_fds.erase(fd);
+        update_env();
+        return fd;
     }
 
     if (fds.empty())
         return std::nullopt;
 
     int fd = fds.front();
-    fds.pop();
+    fds.pop_front();
+    all_fds.erase(fd);
+    update_env();
     return fd;
 }
 
 /* Check whether the given file descriptor is passed by systemd. */
 bool Systemd::has_fd(int fd)
 {
-    return fd >= SD_LISTEN_FDS_START && fd < SD_LISTEN_FDS_START + fd_count;
+    return all_fds.find(fd) != all_fds.end();
 }

src/systemd.hh
@@ -2,9 +2,11 @@
 #ifndef IP2UNIX_SYSTEMD_HH
 #define IP2UNIX_SYSTEMD_HH
 
+#include "rules.hh"
+
 namespace Systemd {
-    void init(void);
-    std::optional<int> get_fd_for_rule(const Rule&);
+    void init(const std::vector<Rule>&);
+    std::optional<int> acquire_fd_for_rulepos(size_t);
     bool has_fd(int);
 }
 

commit 5ead045bffe781e1133d3881d70a4b5bdc66021d

preload: Fix incrementing rule position
Regression introduced by 1a8a9b6ca73865a10452eb10c93dd751d0ba6af7.

The commit in question overhauled the way we do rule matching for
systemd sockets, but when changing the match_rule function to return the
rule position in the parent commit (1761a000f5159a8fef449eabf1e1d10e84),
I forgot to actually increment the variable.

For normal actions, the rule position doesn't matte much because we
immediately perform the action in case of a match. For systemd rules on
the other hand, we need to know whether we need the rule position
matches with the corresponding systemd socket.

This and the fact that we only had tests for single systemd sockets was
the reason why we did not catch this earlier. To avoid future
regressions, I added a small NixOS VM test with multiple socket fds.

Signed-off-by: aszlig <aszlig@nix.build>

release.nix
@@ -164,11 +164,14 @@ let
     hardeningDisable = [ "all" ];
   });
 
-  tests.vm = {
-    systemd = lib.genAttrs systems (system: (import ./tests/vm/systemd.nix {
+  tests.vm = let
+    callTest = path: lib.genAttrs systems (system: (import path {
       inherit system;
       pkgs = import nixpkgs { inherit system; config = {}; };
     }).test);
+  in {
+    systemd-single = callTest tests/vm/systemd-single.nix;
+    systemd-multi = callTest tests/vm/systemd-multi.nix;
   };
 
   tests.programs = let

src/preload.cc
@@ -158,7 +158,13 @@ static RuleMatch match_rule(const SockAddr &addr, const Socket::Ptr sock,
     init_rules();
 
     size_t rulepos = 0;
-    for (auto &rule : *g_rules) {
+    for (
+        std::vector<Rule>::const_iterator it = g_rules->begin();
+        it != g_rules->end();
+        ++it, ++rulepos
+    ) {
+        const Rule &rule = *it;
+
         if (rule.direction && rule.direction != dir)
             continue;
 

tests/vm/systemd-multi.nix
@@ -1,0 +1,96 @@
+import <nixpkgs/nixos/tests/make-test-python.nix> {
+  name = "ip2unix-systemd-multi";
+
+  machine = { pkgs, lib, ... }: let
+    ip2unix = import ../.. { inherit pkgs lib; };
+
+    testServer = pkgs.writeScript "test-server.py" ''
+      #!${pkgs.python3.interpreter}
+      from functools import partial
+      from selectors import DefaultSelector, EVENT_READ
+      from http.server import BaseHTTPRequestHandler, HTTPServer
+
+      class TestHandler(BaseHTTPRequestHandler):
+        def __init__(self, *args, reply, **kwargs):
+          self.reply = reply
+          super().__init__(*args, **kwargs)
+
+        def do_GET(self):
+          self.send_response(200)
+          self.send_header('Content-Type', 'text/plain')
+          self.send_header('X-Reply', self.reply)
+          self.end_headers()
+          self.wfile.write(self.reply.encode())
+
+      selector = DefaultSelector()
+
+      # Note that it's *intentional* here that the ports are in reverse order
+      # than they are in the ip2unix invocation, because otherwise we'd get
+      # rules matched in order.
+      for port in [1113, 8090]:
+        handler = partial(TestHandler, reply=f'port{port}')
+        server = HTTPServer(('127.0.0.1', port), handler)
+        selector.register(server, EVENT_READ, server.handle_request)
+
+      while True:
+        for event in selector.select():
+          event[0].data()
+    '';
+
+  in {
+    systemd.sockets.testsock1 = {
+      description = "Test Socket 1 for Multiple Socket Test";
+      wantedBy = [ "sockets.target" ];
+      requiredBy = [ "socktest.service" ];
+      socketConfig.ListenStream = "/run/test1.sock";
+      socketConfig.FileDescriptorName = "socket1";
+      socketConfig.Service = "socktest.service";
+    };
+
+    systemd.sockets.testsock2 = {
+      description = "Test Socket 2 for Multiple Socket Test";
+      wantedBy = [ "sockets.target" ];
+      requiredBy = [ "socktest.service" ];
+      socketConfig.ListenStream = "/run/test2.sock";
+      socketConfig.FileDescriptorName = "socket2";
+      socketConfig.Service = "socktest.service";
+    };
+
+    systemd.services.canary = {
+      description = "Canary Service to verify implementation";
+      after = [ "network.target" ];
+      requiredBy = [ "multi-user.target" ];
+      serviceConfig.User = "testuser";
+      serviceConfig.Group = "testgroup";
+      serviceConfig.ExecStart = testServer;
+    };
+
+    systemd.services.socktest = {
+      description = "Test Service for Multiple Socket Test";
+      serviceConfig.User = "testuser";
+      serviceConfig.Group = "testgroup";
+      serviceConfig.ExecStart = lib.escapeShellArgs [
+        "${ip2unix}/bin/ip2unix"
+        "-r" "in,tcp,port=8090,systemd=socket1"
+        "-r" "in,tcp,port=1113,systemd=socket2"
+        "-r" "out,ignore"
+        testServer
+      ];
+    };
+
+    users.users.testuser.isSystemUser = true;
+    users.users.testuser.group = "testgroup";
+    users.groups.testgroup = {};
+  };
+
+  testScript = ''
+    # fmt: off
+    machine.wait_for_unit('multi-user.target')
+
+    for n, port in enumerate([8090, 1113], start=1):
+      for args in [f'http://127.0.0.1:{port}/',
+                   f'--unix-socket /run/test{n}.sock http://test/']:
+        cmd = f'test "$(curl --no-progress-meter -v {args})" = port{port}'
+        machine.succeed(f'if ! {cmd}; then sleep 20; exit 1; fi')  # XXX
+  '';
+}