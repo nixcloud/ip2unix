2833273873d6a5e134618acd9cb37e913e7bd90d
aszlig <aszlig@nix.build>
2023-08-08 04:15:01 +0200

    tests/abstract: Use UUID for the abstract name
    The repeat100 Hydra jobsets fail regularly with errors like these:
    
          @abstract_sockets_only
          def test_abstcact_socket():
              with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
      >           sock.bind('\0abstest')
      E           OSError: [Errno 98] Address already in use
    
    This of course is because we always use the same abstract socket name,
    which at some point might not be cleaned up before the next test run
    starts, resulting in the above error.
    
    Simply using an UUID as the name should avoid this.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- tests/test_abstract_socket.py
+++ tests/test_abstract_socket.py
@@ -2,6 +2,7 @@ import socket
 import subprocess
 import sys
 
+from uuid import uuid4
 from helper import abstract_sockets_only, IP2UNIX
 
 TESTPROG = r'''
@@ -16,12 +17,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
 @abstract_sockets_only
 def test_abstcact_socket():
+    name = uuid4().hex
+
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-        sock.bind('\0abstest')
+        sock.bind('\0' + name)
         sock.listen(10)
 
         cmd = [IP2UNIX]
-        cmd += ['-r', 'addr=4.3.2.1,abstract=abstest']
+        cmd += ['-r', f'addr=4.3.2.1,abstract={name}']
         cmd += [sys.executable, '-c', TESTPROG]
 
         with subprocess.Popen(cmd, stdout=subprocess.PIPE), \
