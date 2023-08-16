0b802c9f14c3b8c849a5788b8bb4bcf41a3c2ac1
aszlig <aszlig@nix.build>
2023-08-16 16:40:28 +0200

    Remove default.nix with flake-compat
    Our build instructions already refer to Flakes and all supported Nix
    versions nowadays allow to use them behind a feature-flag, so there
    really is no reason to keep around this file.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- default.nix
+++
@@ -1,6 +0,0 @@
-let
-  rev = "99f1c2157fba4bfe6211a321fd0ee43199025dbf";
-  url = "https://github.com/edolstra/flake-compat/archive/${rev}.tar.gz";
-  flake = import (fetchTarball url) { src = ./.; };
-  inNixShell = builtins.getEnv "IN_NIX_SHELL" != "";
-in if inNixShell then flake.shellNix else flake.defaultNix
